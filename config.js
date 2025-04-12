75b71a1eb631699300ee10df3b4926615b7cc410
Victor Olorunda <85253574+KeithV978@users.noreply.github.com>
2025-04-12 12:20:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 const TelegramBot = require('node-telegram-bot-api');
 
 // Load environment variables
-const result = dotenv.config();
-if (result.error) {
-    throw new Error('Error loading .env file');
-}
+//const result = dotenv.config();
+//if (result.error) {
+    //throw new Error('Error loading .env file');
+// }
 
 if (!process.env.BOT_TOKEN) {
     throw new Error('BOT_TOKEN is not defined in environment variables');
