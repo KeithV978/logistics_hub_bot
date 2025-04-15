5ecb6e7caaee7dd171cb449491ef76de79752d8f
victor olorunda <victorolorunda6@gmail.com>
2025-04-15 17:35:11 +0100

    debugging

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { Telegraf } = require('telegraf');
 
-const bot = new Telegraf(proccess.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.command('start', (ctx) => ctx.reply('Hello World'));
 
 bot.launch({
   webhook: {
-    domain: proccess.env.WEBHOOK_DOMAIN,
-    hookPath: proccess.env.WEBHOOK_PATH,
+    domain: process.env.WEBHOOK_DOMAIN,
+    hookPath: process.env.WEBHOOK_PATH,
     port: 3000
   }
 }).then(() => {
