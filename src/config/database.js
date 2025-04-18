3524322b18de97ed3e4f4950622da046c31e0b42
victor olorunda <victorolorunda6@gmail.com>
2025-04-18 21:49:15 +0100

    fixed initial error. Now let's see if it'll rrun on server

--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,14 @@ const path = require('path');
 const { Sequelize } = require('sequelize');
 // const config = require('../../config/database');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "postgres",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Required for Render's PostgreSQL
+    }
+  },
   logging: true,
   pool: true,
 });
