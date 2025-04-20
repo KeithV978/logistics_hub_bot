608558de03d82da5f1b5cfbefa5882a6701bca2b
victor olorunda <victorolorunda6@gmail.com>
2025-04-20 18:23:42 +0100

    greeter done

--- src/utils/Greeting.js
+++ src/utils/Greeting.js
@@ -2,11 +2,11 @@ function userGreeting(user) {
     const currentHour = new Date().getHours();
     
     if (currentHour >= 12 && currentHour < 18) {
-      return "Good Afternoon" + user;
+      return `Good Afternoon  ${user}`;
     } else if (currentHour >= 18 || currentHour < 5) {
-      return "Good Evening" + user;
+      return `Good Evening  ${user}`;
     } else {
-      return "Good Morning" + user;
+      return `Good Morning  ${user}`;
     }
   }
 
